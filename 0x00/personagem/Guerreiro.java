d2231ef2d2bbfce23f7d5e78ca85562fec72b7b3
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-05-02 17:54:50 -0300

    0. Factory Method de Personagens

--- 0x00/personagem/Guerreiro.java
+++ 0x00/personagem/Guerreiro.java
@@ -11,7 +11,7 @@ public class Guerreiro extends Personagem{
     public Guerreiro(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
         super(nome, TipoPersonagem.GUERREIRO, inteligencia, forca, vigor, resistencia, destreza);
 
-        if(forca > inteligencia && forca > destreza){
+        if(forca < inteligencia && forca < destreza){
             throw new RuntimeException("Atributos invalidos para GUERREIRO");
         }
     }
--- 0x00/personagem/Ladrao.java
+++ 0x00/personagem/Ladrao.java
@@ -11,8 +11,8 @@ public class Ladrao extends Personagem{
     public Ladrao(String nome, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
         super(nome, TipoPersonagem.LADRAO, inteligencia, forca, vigor, resistencia, destreza);
 
-        if(destreza > inteligencia && destreza > forca){
-            throw new RuntimeException("Atributos invalidos para GUERREIRO");
+        if(destreza < inteligencia || destreza < forca){
+            throw new RuntimeException("Atributos invalidos para LADRAO");
         }
     }
 }

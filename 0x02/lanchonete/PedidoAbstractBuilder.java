b6d1462c149fcf2712fc941a6ece0149e84b68ae
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-05-03 08:45:03 -0300

    0. Serviço Lanchonete Pedido Builder

---
+++ 0x02/lanchonete/PedidoAbstractBuilder.java
@@ -0,0 +1,10 @@
+public abstract class PedidoAbstractBuilder {
+
+    void setLanche(TipoLanche tipo) {}
+
+    void setBatata(TamanhoBatata tamanho) {}
+
+    void setBrinde(TipoBrinde tipo) {}
+
+    void setBebida(TipoBebida tipo) {}
+}

4a897deb5120946661f258296adf2c426ca048bc
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-05-03 12:17:15 -0300

    0. Guerra de Naves Espaciais

---
+++ 0x03/naves/NaveEspacial.java
@@ -0,0 +1,31 @@
+public class NaveEspacial {
+
+    private int saude;
+    private int ataque;
+
+    public NaveEspacial(int saude, int ataque) {
+        this.saude = saude;
+        this.ataque = ataque;
+    }
+
+    public NaveEspacial() {
+    }
+
+    public NaveEspacial(NaveEspacial naveEspacial) {
+    }
+
+
+    public int getSaude() {
+        return saude;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Saude: %d - Ataque: %d", getSaude(), getAtaque());
+    }
+}
